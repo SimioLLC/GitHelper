47572043cdd53f466fde590879050bb4576fc019
Daniel Houck <dhouck@simio.com>
2024-07-12 13:22:34 -0400

    Modifying to get correct resolution of the userProfile. E.g. under c:\users or under OneDrive.

--- Source/GetDocumentsFolder.ps1
+++ Source/GetDocumentsFolder.ps1
@@ -1,2 +1,4 @@
 # Get the documents folder, regardless of whether the user profile points to the users folder on the OS drive or OneDrive
-[Environment]::GetFolderPath('MyDocuments')
+$documentsPath = [Environment]::GetFolderPath('MyDocuments')
+Write-Output "DocPath= $documentsPath"
+Write-Output $documentsPath | Out-File -FilePath "$(Get-Location)\DocumentsPath.txt"
